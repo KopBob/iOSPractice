dd3757768e3eee2db6e2c5ee38db61868506482a
Kopin Boris <kopbob@MacBook-Pro-Egor.local>
2012-09-08 19:47:10 +0400

    Empty Project

---
+++ iOSPractice/iOSPractice/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iOSPractice
+//
+//  Created by Kopin Boris on 08.09.12.
+//  Copyright (c) 2012 Kopin Boris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "KBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([KBAppDelegate class]));
+    }
+}
